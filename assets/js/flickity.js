3065e60b7ab4f00d37ca0992dbe436bafa23fb16
Michał Białkowski <michal@bialkowski.net.pl>
2020-03-17 13:02:24 +0100

    Update

--- assets/js/flickity.js
+++ assets/js/flickity.js
@@ -1,10 +1,40 @@
 $(window).on('load', function() {
-	$('.flickity--slider').flickity({
-		cellAlign: 'center',
-		contain: true,
-		freeScroll: false,
-		wrapAround: true,
-		pageDots: true,
-		autoPlay: true
+	$('.flickity--slider').each(function() {
+		var options = {
+			contain: true,
+			freeScroll: false,
+			adaptiveHeight: false,
+			autoPlay: true
+		};
+		
+		if ($(this).hasClass('justify-content-start')) {
+			options.cellAlign = 'left';
+		} else if ($(this).hasClass('justify-content-center')) {
+			options.cellAlign = 'center';
+		} else if ($(this).hasClass('justify-content-end')) {
+			options.cellAlign = 'right';
+		} else {
+			options.cellAlign = 'left';
+		}
+		
+		if ($(this).hasClass('flickity--dots')) {
+			options.pageDots = true;
+		} else {
+			options.pageDots = false;
+		}
+		
+		if ($(this).hasClass('flickity--nav')) {
+			options.prevNextButtons = true;
+		} else {
+			options.prevNextButtons = false;
+		}
+		
+		if ($(this).hasClass('flickity--loop')) {
+			options.wrapAround = true;
+		} else {
+			options.wrapAround = false;
+		}
+		
+		$(this).flickity(options);
 	});
 });
